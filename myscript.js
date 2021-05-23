82d94a207368d1e264128bfd9e3f3acf237fbfd5
shaaz1998 <kmdshaaz8@gmail.com>
2021-05-23 08:35:21 +0530

    update

---
+++ myscript.js
@@ -0,0 +1,14 @@
+var inputText = document.querySelector("#input-txt");
+var inputButton = document.querySelector("#input-btn");
+
+
+
+
+// function clickeventhandler()
+
+inputButton.addEventListener("click", function clickevenhandler() {
+    console.log("clicked");
+    console.log(inputText.value);
+
+});
+
