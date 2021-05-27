fe22af340acd27fcaab1797c233134c30e5b2f29
shaaz1998 <kmdshaaz8@gmail.com>
2021-05-27 10:12:40 +0530

    update

--- myscript.js
+++ myscript.js
@@ -1,14 +1,23 @@
 var inputText = document.querySelector("#input-txt");
 var inputButton = document.querySelector("#input-btn");
+var outputText = document.querySelector("#output-txt");
 
 
 
 
 // function clickeventhandler()
-
-inputButton.addEventListener("click", function clickevenhandler() {
+function clickevenhandler() {
     console.log("clicked");
     console.log(inputText.value);
+    
+    // alert("welcome "+inputText.value);
+    outputText.innerText=inputText.value;
+    console.log(outputText);
+    console.log(outputText.innerText);
+    
 
-});
+}
+
+inputButton.addEventListener("click",clickevenhandler );
+
 
