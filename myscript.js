771339516435c3296208608d2e74020ecdb96e52
shaaz1998 <kmdshaaz8@gmail.com>
2021-05-28 17:02:18 +0530

    update

--- myscript.js
+++ myscript.js
@@ -1,20 +1,26 @@
 var inputText = document.querySelector("#input-txt");
 var inputButton = document.querySelector("#input-btn");
 var outputText = document.querySelector("#output-txt");
+var serverURL ="http://127.0.0.1:5500/index.html";
 
-
+function getTranslationURL(text) {
+    return serverURL + "?"+ "text" + text
+}
 
 
 // function clickeventhandler()
 function clickevenhandler() {
-    console.log("clicked");
-    console.log(inputText.value);
+    // console.log("clicked");
+    // console.log(inputText.value);
     
     // alert("welcome "+inputText.value);
-    outputText.innerText=inputText.value;
-    console.log(outputText);
-    console.log(outputText.innerText);
-    
+    // outputText.innerText=inputText.value;
+    // console.log(outputText);
+    // console.log(outputText.innerText);
+    ouputText = input-txt.value;
+    fetch(getTranslationURL(inputText))
+        .then(response =>response.json())
+        .then(json => console.log(json))
 
 }
 
